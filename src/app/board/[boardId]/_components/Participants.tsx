0b5bcfb5e75f036e3e6ceb75241f163ea027b76a
Piyush Jain <piyushjz0402@gmail.com>
2024-05-14 00:26:45 +0530

    user avatars and toolbar

--- src/app/board/[boardId]/_components/Participants.tsx
+++ src/app/board/[boardId]/_components/Participants.tsx
@@ -1,7 +1,46 @@
+'use client';
+
+import { connectionIdToColor } from '@/lib/utils';
+import { useOthers, useSelf } from '@/liveblocks.config';
+import UserAvatar from './UserAvatar';
+
+const MAX_SHOWN_USERS = 4;
+
 const Participants = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_USERS;
+
   return (
     <div className='absolute right-2 top-2 flex h-12 items-center rounded-md bg-white p-3 shadow-md'>
-      List of Users
+      <div className='flex gap-x-2'>
+        {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }) => (
+          <UserAvatar
+            key={connectionId}
+            src={info.picture}
+            name={info.name}
+            fallback={info.name?.[0] ?? 'T'}
+            borderColor={connectionIdToColor(connectionId)}
+          />
+        ))}
+        {currentUser ? (
+          <UserAvatar
+            src={currentUser.info.picture}
+            name={`${currentUser.info.name} (You)`}
+            fallback={currentUser.info.name?.[0]}
+          />
+        ) : (
+          <></>
+        )}
+        {hasMoreUsers ? (
+          <UserAvatar
+            name={`${users.length - MAX_SHOWN_USERS} more`}
+            fallback={`+${users.length - MAX_SHOWN_USERS}`}
+          />
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,31 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+const COLORS = [
+  '#023436',
+  '#F03A47',
+  '#96C5F7',
+  '#8884FF',
+  '#40F99B',
+  '#FFD046',
+  '#DA7422',
+  '#C2F261',
+  '#51BBFE',
+  '#8FF7A7',
+  '#AF4D98',
+  '#2F4858',
+  '#FF499E',
+  '#49B6FF',
+  '#A480CF',
+  '#C7EF00',
+  '#FF1D15',
+  '#1A1B41',
+  '#3B60E4',
+  '#A846A0',
+  '#F0386B',
+];
+
+export function connectionIdToColor(id: number) {
+  return COLORS[id % COLORS.length];
+}
